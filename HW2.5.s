10974b3a9de9c8f4460a931d95a3434aba33976f
s-regan <sarahregan12349@gmail.com>
2019-06-01 14:06:00 -0600

    Update HW2.5.s
    added program from notes, works

--- HW2.5.s
+++ HW2.5.s
@@ -1,3 +1,26 @@
 @Sarah Regan, 2017
 
 @count number of 't's in string
+
+string: .asciz "this is my string" 	@count should be 17
+char:	.ascii "t"	@count should be 2
+
+	.equ nul,0
+	.align
+	
+	LDR R0,=string
+	LDR R1,=char
+	MOV R2,#0
+	LDRB R4,[R1],#1
+	
+loop: LDRB R3,[R0],#1
+	CMP R3,#nul
+	BEQ stop
+	CMP R4,R3
+	BNE loop
+	
+	ADD R2,R2,#1
+	CMP R3,#nul
+	BNE loop
+	
+stop: SWI 0x11
