a7d66f7bb0fbc133b44cd65eabb2320b2f8a04e7
s-regan <sarahregan12349@gmail.com>
2019-05-31 18:16:33 -0600

    Update HW2.s
    notepad++ hates me, format fix 3

--- HW2.s
+++ HW2.s
@@ -11,13 +11,13 @@ string: .asciz "This is my string"	@count should be 17
 	LDR R0,=string		@load whatever sting is into R0
 	MOV R1,#0		@move 0 into R1
 	
-loop: 		LDRB R2,[R0],#1			@load 1 byte from R0(char) into R2
+loop: 		LDRB R2,[R0],#1		@load 1 byte from R0(char) into R2
 	ADD R1,R1,#1		@increment counter by 1
 	CMP R2,#nul		@compare R2 to nothing 
 	BNE loop		@if false goto loop
 		
 	SUB R1,R1,#1		@if true, subtract 1 from counter
 
-stop:	SWI 0x11			@stop program
+stop:	SWI 0x11		@stop program
 
 @if using ARMSim, R1 should be equal to 11 (hex for 17)
