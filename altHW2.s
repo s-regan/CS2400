fa418737706464910413af331ccb5995249571fa
s-regan <sarahregan12349@gmail.com>
2019-06-02 21:57:48 -0600

    Update altHW2.s
    format update

--- altHW2.s
+++ altHW2.s
@@ -4,19 +4,19 @@
 
 	.equ SWI_exit, 0x11
 
-start: LDR R0,=a
+start:	LDR R0,=a
 	LDR R1,[R0]
 	LDR R0,=b
 	LDR R2,[R0]
 	MOV R3,R1
 	
-loop: CMP R3,R2
+loop:	CMP R3,R2
 	BGT end
 	
 	ADD R3,R3,#1
 	B loop
 	
-end: LDR R0,=c
+end:	LDR R0,=c
 	STR R3,[R0]
 	
 	SWI SWI_exit
